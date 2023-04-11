912c0c06ecc4bed133d895469e0e255c31ca131b
PIYUSH WADHAWAN <wa.piyush@gmail.com>
2023-04-12 02:44:19 +0530

    Typing effect added

--- src/Components/home/Data.jsx
+++ src/Components/home/Data.jsx
@@ -2,18 +2,22 @@ import React from 'react'
 import { GiHand } from "react-icons/gi"
 import { MdEmojiPeople } from "react-icons/md"
 import { HiDocument } from "react-icons/hi"
+import Resume from "../../Data/Resume.pdf"
 
 const Data = () => {
   return (
     <div className="home-data">
         <h1 className="home-title">Piyush Wadhawan <span className="home-hand"><GiHand/></span></h1>
-        <h3 className="home-subtitle">Front-End Developer</h3>
+        {/* <h3 className="home-subtitle">I'am</h3> */}
+        <span className="typewriter">
+          <h3 className="home-subtitle">I'am a Front-End Developer</h3>
+        </span>
         <p className="home-description">I am a creative entry level developer, passionate about working on projects with other talent, contribute ideas and learn new things</p>
         <div className="home-btns">   
             <a href="#contact" className="button button-flex btn-size">
                 Say Hello <MdEmojiPeople/>
             </a>
-            <a href="https://drive.google.com/file/d/1Ta3QVtwA1P2r0V05ZzBts_mTQUXib1h8/view" className="button button-flex btn-light btn-size" target='_blank' rel="noreferrer">
+            <a download={Resume} href={Resume} className="button button-flex btn-light btn-size" target='_blank' rel="noreferrer">
                 Resume <HiDocument/>
             </a>
         </div>
