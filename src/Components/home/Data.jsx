96e2ccee17a6be67e5ef2375c62d44e910f53082
PIYUSH WADHAWAN <wa.piyush@gmail.com>
2023-04-11 16:32:42 +0530

    Made home responsive

--- src/Components/home/Data.jsx
+++ src/Components/home/Data.jsx
@@ -10,10 +10,10 @@ const Data = () => {
         <h3 className="home-subtitle">Front-End Developer</h3>
         <p className="home-description">. I am a creative entry level developer, passionate about working on projects with other talent, contribute ideas and learn new things</p>
         <div className="home-btns">   
-            <a href="#contact" className="button button-flex">
+            <a href="#contact" className="button button-flex btn-size">
                 Say Hello <MdEmojiPeople/>
             </a>
-            <a href="https://drive.google.com/file/d/1Ta3QVtwA1P2r0V05ZzBts_mTQUXib1h8/view" className="button button-flex btn-light" target='_blank' rel="noreferrer">
+            <a href="https://drive.google.com/file/d/1Ta3QVtwA1P2r0V05ZzBts_mTQUXib1h8/view" className="button button-flex btn-light btn-size" target='_blank' rel="noreferrer">
                 Resume <HiDocument/>
             </a>
         </div>
