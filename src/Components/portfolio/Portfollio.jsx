149fd878c717b0df6697e205cd48d4efd84f4192
PIYUSH WADHAWAN <wa.piyush@gmail.com>
2023-04-15 18:23:42 +0530

    removed more errors

--- src/Components/portfolio/Portfollio.jsx
+++ src/Components/portfolio/Portfollio.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './Portfolio.css'
 import PortfolioList from './PortfolioList'
 import project_data from '../../Data/Portfolio-data' 
@@ -6,7 +6,7 @@ import Pagination from './Pagination'
 
 const Portfollio = () => {
 
-    const [projects, setProject] = useState(project_data);
+    const projects = project_data;
 
     const [ currentPage, setCurrentPage ] = useState(1);
     const [ cardsPerPage, setCardPerPage ] = useState(4); 
@@ -16,28 +16,28 @@ const Portfollio = () => {
 
     const currentProjects = projects.slice(firstCardIndex, lastCardIndex);
 
-    // const [windowSize, setWindowSize] = useState([
-    //   window.innerWidth,
-    //   window.innerHeight,
-    // ]);
+    const [windowSize, setWindowSize] = useState([
+      window.innerWidth,
+      window.innerHeight,
+    ]);
 
-    // useEffect(() => {
-    //   const handleWindowResize = () => {
-    //     setWindowSize([window.innerWidth, window.innerHeight]);
-    //   };
+    useEffect(() => {
+      const handleWindowResize = () => {
+        setWindowSize([window.innerWidth, window.innerHeight]);
+      };
 
-    //   window.addEventListener('resize', handleWindowResize);
-    //   if(windowSize[0]<=1100) {
-    //     setCardPerPage(2);
-    //   }
-    //   else {
-    //     setCardPerPage(4);
-    //   }
+      window.addEventListener('resize', handleWindowResize);
+      if(windowSize[0]<=1100) {
+        setCardPerPage(2);
+      }
+      else {
+        setCardPerPage(4);
+      }
 
-    //   return () => {
-    //     window.removeEventListener('resize', handleWindowResize);
-    //   };
-    // });
+      return () => {
+        window.removeEventListener('resize', handleWindowResize);
+      };
+    }, [windowSize]);
 
 
   return (
